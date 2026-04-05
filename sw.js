497d51ddac429acd61deade63918f110df6a7285
Anshu Gupta <anshuraj77346@gmail.com>
2026-04-05 23:58:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    ADHYAYAN CLASSES — sw.js  (Service Worker)
    ========================================== */
 
-const CACHE_NAME = 'adhyayan-v7';
+const CACHE_NAME = 'adhyayan-v8';
 
 const PRECACHE = [
   './',
